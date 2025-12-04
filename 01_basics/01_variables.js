ef1ff839ea2d60fa292177ef9bf5eea8f049080b
Minhaj Arif <minhajarif098@gmail.com>
2025-12-04 19:03:51 +0000

    aa

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+
+const accountId = 144553
+let accountEmail =  "minhaj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaupur"
+
+accountEmail =  "min@gmail.com"
+console.log(accountId);
+console.log(accountEmail);
