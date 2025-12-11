a644df0c63a7305b602c69d01da677cc6397d464
Minhaj Arif <minhajarif098@gmail.com>
2025-12-11 12:45:27 +0530

    Delete 01_basics directory

--- 01_basics/01_variables.js
+++
@@ -1,9 +0,0 @@
-
-const accountId = 144553
-let accountEmail =  "minhaj@gmail.com"
-var accountPassword = "12345"
-accountCity = "Jaupur"
-
-accountEmail =  "min@gmail.com"
-console.log(accountId);
-console.log(accountEmail);
